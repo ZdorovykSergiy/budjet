3a7e5745a7617787d2c5172b157dfc3c1b47602b
ZdorovykSergiy <ZdorovykSergiy@gmail.com>
2019-08-22 20:48:03 +0300

    Add files via upload

---
+++ js/js2/javascript.js
@@ -0,0 +1,10 @@
+"use script";
+
+let money = prompt("Ваш бюджет в месяц?");
+let timeData1 = prompt("Введите дату в формате YYYY-MM-DD");
+
+let appData {
+    budjet: money,
+    timeData: timeData1,
+
+}
