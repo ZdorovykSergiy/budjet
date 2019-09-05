9f500eeb4a681e0323bc268623b6cba3845694e5
ZdorovykSergiy <ZdorovykSergiy@gmail.com>
2019-09-05 19:36:33 +0300

    Add files via upload

---
+++ js/lesson5/src/js/main.js
@@ -0,0 +1,27 @@
+let start = document.getElementById("start"),
+  budgetValue = document.getElementsByClassName("budget-value"),
+  daybudgetValue = document.getElementsByClassName("daybudget-value"),
+  levelValue = document.getElementsByClassName("level-value"),
+  optionalexpensesValue = document.getElementsByClassName(
+    "optionalexpenses-value"
+  ),
+  expensesValue = document.getElementsByClassName("expenses-value"),
+  incomeValue = document.getElementsByClassName("income-value"),
+  monthsavingsValue = document.getElementsByClassName("monthsavings-value"),
+  yearsavingsValue = document.getElementsByClassName("yearsavings-value"),
+  expensesItem = document.getElementsByClassName("expenses-item"),
+  btn = document.getElementsByTagName("button"),
+  optionalexpensesBtn = btn[0],
+  countBudgetBtn = btn[1],
+  optionalexpensesItem = document.querySelectorAll(".optionalexpenses-item"),
+  chooseIncome = document.querySelector(".choose-income"),
+  checksavings = document.getElementById("savings"),
+  chooseSum = document.querySelector(".choose-sum"),
+  choosePercent = document.querySelector(".choose-percent"),
+  yearValue = document.querySelector(".year-value");
+monthValue = document.querySelector(".month-value");
+dayValue = document.querySelector(".day-value");
+console.log(optionalexpensesBtn);
+console.log(start);
+console.log(dayValue);
+console.log(checksavings);
